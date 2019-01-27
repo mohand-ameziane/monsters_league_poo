1320b443548c636984c70ea5e36d0e6d1cb33f36
bezza <bezza0624@gmail.com>
2019-01-28 00:37:42 +0100

    mon premier projet git

---
+++ monsters_league_poo/functions.php
@@ -0,0 +1,74 @@
+<?php
+
+function getMonstersObjet(){
+    $monsters = getMonsters();
+    $monstersObjet = array();
+
+    foreach($monsters as $monster){
+        $monsterObjet[] = new monster($monster['name'],$monster['strength'],$monster['life'],$monster['type']);
+    }
+
+    return $monsterObjet;
+}
+
+function getMonsters()
+{
+    return [
+        [
+            'name' => 'Domovoï',
+            'strength' => 30,
+            'life' => 300,
+            'type' => 'water'
+        ],
+        [
+            'name' => 'Wendigos',
+            'strength' => 100,
+            'life' => 450,
+            'type' => 'earth'
+        ],
+        [
+            'name' => 'Thunderbird',
+            'strength' => 400,
+            'life' => 500,
+            'type' => 'air'
+        ],
+        [
+            'name' => 'Sirrush',
+            'strength' => 250,
+            'life' => 1500,
+            'type' => 'fire'
+        ],
+    ];
+}
+
+/**
+ * Our complex fighting algorithm!
+ *
+ * @return array With keys winning_ship, losing_ship & used_jedi_powers
+ */
+function fight(array $firstMonster, array $secondMonster)
+{
+    $firstMonsterLife = $firstMonster['life'];
+    $secondMonsterLife = $secondMonster['life'];
+
+    while ($firstMonsterLife > 0 && $secondMonsterLife > 0) {
+        $firstMonsterLife = $firstMonsterLife - $secondMonster['strength'];
+        $secondMonsterLife = $secondMonsterLife - $firstMonster['strength'];
+    }
+
+    if ($firstMonsterLife <= 0 && $secondMonsterLife <= 0) {
+        $winner = null;
+        $looser = null;
+    } elseif ($firstMonsterLife <= 0) {
+        $winner = $secondMonster;
+        $looser = $firstMonster;
+    } else {
+        $winner = $firstMonster;
+        $looser = $secondMonster;
+    }
+
+    return array(
+        'winner' => $winner,
+        'looser' => $looser,
+    );
+}
